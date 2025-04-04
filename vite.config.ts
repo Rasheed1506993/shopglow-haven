d960ea466c6ef8b004e79ed3fa64c9b29de84a12
Rasheed Sharaf Aldeen <134731625+Rasheed1506993@users.noreply.github.com>
2025-04-04 20:54:55 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,17 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
- base: '/shopglow-haven/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
+   
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+ base: '/shopglow-haven/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
