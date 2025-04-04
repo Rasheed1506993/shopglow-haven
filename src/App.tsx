eda69cc17a585bc50dc34370f24e26648e9e6361
Rasheed Sharaf Aldeen <134731625+Rasheed1506993@users.noreply.github.com>
2025-04-04 20:39:00 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/shopglow-haven">
             <div className="flex flex-col min-h-screen">
               <Navbar />
               <main className="flex-grow pt-20">
